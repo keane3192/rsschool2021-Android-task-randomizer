516d1901b2343e49a8ee7e5299331ebfc14982e9
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-05-30 14:27:12 +0300

    feat: added functionality

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -7,7 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements OnDataPass {
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
         openFirstFragment(0);
     }
 
-    private void openFirstFragment(int previousNumber) {
+
+
+    public void openFirstFragment(int previousNumber) {
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment);
-        // TODO: invoke function which apply changes of the transaction
+        transaction.commit();
+
     }
 
-    private void openSecondFragment(int min, int max) {
-        // TODO: implement it
+    public void openSecondFragment(int min, int max) {
+        final Fragment secondFragment = SecondFragment.newInstance(min, max);
+        final FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+        transaction2.replace(R.id.container, secondFragment);
+        transaction2.commit();
     }
+
 }
